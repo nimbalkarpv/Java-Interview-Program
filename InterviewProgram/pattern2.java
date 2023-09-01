9b75aecb30ca980ba09c9c52e5ccffda88023d98
nimbalkarpv <65148764+nimbalkarpv@users.noreply.github.com>
2023-09-01 18:17:36 +0530

    added pattern2

---
+++ InterviewProgram/pattern2.java
@@ -0,0 +1,23 @@
+package pattern;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		
+		int row = 4;
+		int colm = 5;
+		for(int i=1; i<=row; i++) {
+			for(int j=1; j<=colm; j++) {
+				
+				if(i == 1 || j == 1 || i == row ||j == colm  ) {
+				System.out.print("* ");
+				}else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
