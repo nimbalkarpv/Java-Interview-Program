4e4c9b8d1330f4170e9a0fec23526edb7ef10ee9
nimbalkarpv <65148764+nimbalkarpv@users.noreply.github.com>
2023-09-04 16:01:08 +0530

    added pattern6

---
+++ InterviewProgram/pattern6.java
@@ -0,0 +1,24 @@
+package pattern;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4-i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
+/*
+  
+* * * * 
+* * * 
+* * 
+* 
+  
+ */
