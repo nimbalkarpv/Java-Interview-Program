c6d12e86a26a70818db9a0bd648db2a67295329a
Pratik Nimbalkar <65148764+nimbalkarpv@users.noreply.github.com>
2023-09-18 21:09:11 +0530

    sspattern5

---
+++ InterviewProgram/Pattern/sspattern5.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class sspattern5 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {
+			int k=6;
+			for(int j=1;j<=i;j++) {
+				System.out.print(k+" ");
+				k--;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
