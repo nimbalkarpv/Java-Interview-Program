dc6b827469417bcba3e95ed447c43e153ac83cd2
Pratik Nimbalkar <65148764+nimbalkarpv@users.noreply.github.com>
2023-10-05 20:45:52 +0530

    Add files via upload

---
+++ InterviewProgram/Pattern/spattern10.java
@@ -0,0 +1,27 @@
+package pattern;
+/*
+1 
+1 0 
+1 0 1 
+1 0 1 0 
+1 0 1 0 1 
+1 0 1 0 1 0 
+ */
+
+public class spattern10 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=i;j++) {
+				if(j%2==0) {
+				System.out.print(0+" ");
+				}else {
+					System.out.print(1+" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
