54d38012c7eb3291cb4f3fc9c0403dbd7e0f5ba8
Pratik Nimbalkar <65148764+nimbalkarpv@users.noreply.github.com>
2024-07-08 20:51:29 +0530

    Add files via upload

---
+++ InterviewProgram/Pattern/pattern45.java
@@ -0,0 +1,24 @@
+package pattern;
+/*
+ 
+ * * * * * 
+ * * * * 
+ * * * 	
+ * * 
+ *
+
+*/
+
+public class pattern45 {
+
+	public static void main(String[] args) {
+		for(int i =5;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
