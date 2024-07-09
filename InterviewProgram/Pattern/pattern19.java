aa4a94ac9836220f0104c9ac5394370af0f7c5d7
Pratik Nimbalkar <65148764+nimbalkarpv@users.noreply.github.com>
2024-07-09 22:35:27 +0530

    Add files via upload

---
+++ InterviewProgram/Pattern/pattern19.java
@@ -0,0 +1,22 @@
+package pattern;
+/*
+ 	1 1 1 1 1 1 
+	2 2 2 2 2 2 
+	3 3 3 3 3 3 
+	4 4 4 4 4 4 
+	5 5 5 5 5 5 
+	6 6 6 6 6 6 
+ */
+public class pattern19 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=6;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
